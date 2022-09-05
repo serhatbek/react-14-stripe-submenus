dce88853dae30658ce61ef2efcae290ecfb494be
serhatbek <serhat.bekk@gmail.com>
2022-09-05 12:02:05 +0300

    Hide Submenu

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,8 +13,14 @@ const Navbar = () => {
     openSubMenu(page, { center, bottom });
   };
 
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubMenu();
+    }
+  };
+
   return (
-    <nav className='nav'>
+    <nav className='nav' onMouseOver={handleSubmenu}>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} className='nav-logo' alt='stripe' />
